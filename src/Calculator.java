72f0d1c9057c6912c594044f54c4461c383ea30d
FarhanSial64 <f223681@cfd.nu.edu.pk>
2024-10-10 10:16:40 +0500

    Add and Multply Funtion added

---
+++ src/Calculator.java
@@ -0,0 +1,12 @@
+
+public class Calculator {
+	// Method to add two numbers
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    // Method to multiply two numbers
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+}
